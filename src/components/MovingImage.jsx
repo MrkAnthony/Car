65d11e204e1708d77a2a1bfd9a840ce6bfbff047
MrkAnthony <Jumpcity567@gmail.com>
2024-11-13 19:41:46 -0500

    feat: add additional images to landing page for enhanced visual appeal

--- src/components/MovingImage.jsx
+++ src/components/MovingImage.jsx
@@ -3,14 +3,18 @@ import React, { useState, useEffect } from "react";
 const images = [
     '/Car JPEG/Stock F-150.jpg',
     '/Car JPEG/Jeep Wrangler.jpg',
+    '/Car JPEG/People_Sunset_Car.jpg',
     '/Car JPEG/BlackCorvettes.jpg',
+    '/Car JPEG/BMW_M4.jpg',
     '/Car JPEG/Lamborghini Urus.jpg',
+    '/Car JPEG/Race_Car_Image.jpg',
     '/Car JPEG/Mercedes G-Wagon.jpg',
+    '/Car JPEG/RACE_CAR_PT2.jpg',
     '/Car JPEG/Pink G-Wagon.jpg',
     '/Car JPEG/Mcclaren.jpg',
     '/Car JPEG/Old Corvette.jpg',
     '/Car JPEG/White GTR2.jpg',
-    '/Car JPEG/Mustang.jpg',
+    '/Car JPEG/Mustang.jpg'
 ];
 
 function MovingImage() {
